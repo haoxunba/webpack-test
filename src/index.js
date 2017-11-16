e0809fa27b4d0de2c86ab254833e8d1affd8932a
haoxunba <164008281@qq.com>
2017-11-16 17:40:39 +0800

    init project

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import _ from 'lodash';
+import printMe from './print.js';
+
+function component() {
+  var element = document.createElement('div');
+  var btn = document.createElement('button');
+
+  element.innerHTML = _.join(['Hello', 'web1pack226'], ' ');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+  return element;
+}
+
+document.body.appendChild(component());
+
+ if (module.hot) {
+   module.hot.accept('./print.js', function() {
+     console.log('Accepting the updated printMe mo2ule!');
+     printMe();
+   })
+ }
